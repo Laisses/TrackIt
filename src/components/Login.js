eaa5cdcd219c1a6168384d8cb51e6786d792a15a
Laisses <laisse@gmail.com>
2022-10-18 18:59:24 -0300

    remove import of unused Links

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import logo from "../assets/images/logo.png";
 import { BigButton, LinkContainer, TextInput, Logo } from "./Common";
