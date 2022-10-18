b9cc9e66c70beda5fb395673643b450b1d5f8645
Laisses <laisse@gmail.com>
2022-10-18 16:36:15 -0300

    add some common styled components

---
+++ src/components/Common.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import {LIGHT_BLUE, PRIMARY_FONT, DARK_GREY} from "./constants";
+
+export const BigButton = styled.button`
+    font-family: ${PRIMARY_FONT};
+    font-size: 21px;
+    width: 303px;
+    height: 45px;
+    color: #ffffff;
+    background-color: ${LIGHT_BLUE};
+    border: none;
+    border-radius: 5px;
+`;
+
+export const LinkContainer = styled.div`
+    color: ${LIGHT_BLUE};
+    font-family: ${PRIMARY_FONT};
+    font-size: 14px;
+    text-decoration: underline;
+    margin-top: 25px;
+`;
+
+export const TextInput = styled.input`
+    font-family: ${PRIMARY_FONT};
+    font-size: 20px;
+    color: ${DARK_GREY};
+    width: 303px;
+    height: 45px;
+    border: 1px solid #D5D5D5;
+    border-radius: 5px;
+    margin-bottom: 6px;
+    &::placeholder {
+        color: #DBDBDB;
+        padding-left: 10px;
+    }
+`;
