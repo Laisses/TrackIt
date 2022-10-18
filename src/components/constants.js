dabda75b086e48df9a2d15c1963fe5ac5865f61b
Laisses <laisse@gmail.com>
2022-10-18 15:41:44 -0300

    add secondary font to constants

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,10 +1,11 @@
-export const TEXT_COLOR = "#666666";
-export const TITLE_COLOR = "#126BA5";
+export const DARK_GREY = "#666666";
+export const DARK_BLUE = "#126BA5";
 export const LIGHT_BLUE = "#52B6FF";
 export const BG_COLOR = "#F2F2F2";
 export const LIGHT_GREY = "#CFCFCF";
 export const GREEN = "#8FC549";
 
 export const PRIMARY_FONT = "'Lexend Deca', sans-serif";
+export const SECONDARY_FONT = "'Playball', cursive";
 
 export const BASE_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit";
