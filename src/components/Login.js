c5ed0bd60378e23d43d2b02cfaffbe1f6d5b1a80
Laisses <laisse@gmail.com>
2022-10-18 18:10:10 -0300

    create Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,28 @@
+import styled from "styled-components";
+import logo from "../assets/images/logo.png";
+import { BigButton, LinkContainer, TextInput } from "./Common";
 
 export const Login = () => {    
     return (
-        <div>Sou o Login</div>
+        <LoginContainer>
+            <Logo src={logo} alt="Track It logo" />
+            <TextInput placeholder="email"/>
+            <TextInput placeholder="senha"/>
+            <BigButton>Entrar</BigButton>
+            <LinkContainer>Não tem uma conta? Cadastre-se!</LinkContainer>
+        </LoginContainer>
     );
 };
+
+const LoginContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Logo = styled.img`
+    margin-top: 68px;
+    margin-bottom: 32px;
+    width: 180px;
+    height: 178px;
+`;
