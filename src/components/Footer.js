2eb7848af1cda6d139af855bc8b9f6ba58520506
Laisses <laisse@gmail.com>
2022-10-18 11:25:10 -0300

    add Footer component without spinner

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,34 @@
+import { LIGHT_BLUE, PRIMARY_FONT } from "./constants"
 import styled from "styled-components"
 
 export const Footer = () => {
     return (
-        <Div>Eu sou o Footer</Div>
+        <Div>
+            <span>Hábitos</span>
+            <div>Hoje</div>
+            <span>Historico</span>
+        </Div>
     );
 };
 
 const Div = styled.div`
-    background-color: green;
-    height: 10px;
+    font-family: ${PRIMARY_FONT};
+    font-size: 18px;
+    width: 100%;
+    height: 70px;
+    background-color: #ffffff;
+    color: ${LIGHT_BLUE};
+
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    position: fixed;
+    bottom: 0;
+    right: 0;
+
+    
+
+
+    margin-bottom: 600px;
 `;
 
