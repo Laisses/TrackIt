f804bf275c5e7a63f8f3a5925b32d9fd2e1db417
Laisses <laisse@gmail.com>
2022-10-18 12:06:23 -0300

    create Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,28 @@
 import { LIGHT_BLUE, PRIMARY_FONT } from "./constants"
-import styled from "styled-components"
+import styled from "styled-components";
+import 'react-circular-progressbar/dist/styles.css';
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
 export const Footer = () => {
+    const percentage = 66;
+    
     return (
         <Div>
             <span>Hábitos</span>
-            <div>Hoje</div>
+            <Progress>
+                <CircularProgressbar
+                    value={percentage}
+                    text={`${percentage}%`}
+                    background
+                    backgroundPadding={6}
+                    styles={buildStyles({
+                    backgroundColor: "#52B6FF",
+                    textColor: "#fff",
+                    pathColor: "#fff",
+                    trailColor: "transparent"
+                    })}
+                />
+            </Progress>
             <span>Historico</span>
         </Div>
     );
@@ -18,10 +35,12 @@ const Div = styled.div`
     height: 70px;
     background-color: #ffffff;
     color: ${LIGHT_BLUE};
+    padding-right: 36px;
+    padding-left: 36px;
 
     display: flex;
     align-items: center;
-    justify-content: space-around;
+    justify-content: space-between;
     position: fixed;
     bottom: 0;
     right: 0;
@@ -31,4 +50,16 @@ const Div = styled.div`
 
     margin-bottom: 600px;
 `;
+const Progress = styled.div`
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    bottom: 10px;
+    left: 50%;
+    margin-left: -45px;
+
+    margin-bottom: 600px;
+`;
+
+//Lembrar de tirar os margin-bottom
 
