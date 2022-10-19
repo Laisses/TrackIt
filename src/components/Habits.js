ca424b2974a87e33bb471b8173d898c7e6d44a76
Laisses <laisse@gmail.com>
2022-10-19 14:58:27 -0300

    create layout to displayed habits list

--- src/components/Habits.js
+++ src/components/Habits.js
@@ -3,8 +3,9 @@ import { useState } from "react";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { Title } from "./Common";
-import { LIGHT_BLUE, PRIMARY_FONT, WEEKDAYS } from "./constants";
+import { LIGHT_BLUE, DARK_GREY, PRIMARY_FONT, WEEKDAYS } from "./constants";
 import { TextInput } from "./Common";
+import trashcan from "../assets/images/trashcan.png";
 
 const WEEKDAYS_COLORS = {
     selected: {
@@ -19,9 +20,22 @@ const WEEKDAYS_COLORS = {
     }
 }
 
-export const Habits = () => {    
+const dummyHabits = [
+	{
+		id: 1,
+		name: "Nome do hábito",
+		days: [1, 3, 5]
+	},
+	{
+		id: 2,
+		name: "Nome do hábito 2",
+		days: [1, 3, 4, 6]
+	}
+];
+
+export const Habits = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const [daysChoosen, setDaysChoosen] = useState([]);   
+    const [daysChoosen, setDaysChoosen] = useState([]);
 
     const openEntry = () => {
         if (!isOpen) {
@@ -32,11 +46,12 @@ export const Habits = () => {
     const closeEntry = () => {
         if (isOpen) {
             setIsOpen(false);
+            setDaysChoosen([]);
         }
     };
 
     const handleSelection = (day) => {
-        if(!daysChoosen.includes(day)) {
+        if (!daysChoosen.includes(day)) {
             setDaysChoosen([...daysChoosen, day])
         } else {
             setDaysChoosen(daysChoosen.filter(d => d !== day))
@@ -49,9 +64,9 @@ export const Habits = () => {
                 <TextInput placeholder="nome do hábito" />
                 <DaysInput>
                     {WEEKDAYS.map((d, i) =>
-                        <Day 
+                        <Day
                             key={i}
-                            onClick={() => handleSelection(i)} 
+                            onClick={() => handleSelection(i)}
                             color={daysChoosen.includes(i)
                                 ? WEEKDAYS_COLORS.selected
                                 : WEEKDAYS_COLORS.unselected}>{d}
@@ -59,10 +74,27 @@ export const Habits = () => {
                     )}
                 </DaysInput>
                 <ButtonsContainer>
-                        <CancelButton onClick={closeEntry}>Cancelar</CancelButton>
-                        <SaveButton>Salvar</SaveButton>
+                    <CancelButton onClick={closeEntry}>Cancelar</CancelButton>
+                    <SaveButton>Salvar</SaveButton>
                 </ButtonsContainer>
             </EntryContainer>
+        );
+    };
+
+    const ListOfHabits = ({name, days}) => {
+        return (
+            <ListItem>
+                <Trashcan src={trashcan} alt="ícone de deletar" />
+                <SubTitle>{name}</SubTitle>
+                <DaysInput>
+                    {WEEKDAYS.map((d, i)=> <Day
+                        key={i}
+                        color={days.includes(i)
+                            ? WEEKDAYS_COLORS.selected
+                            : WEEKDAYS_COLORS.unselected}>{d}
+                    </Day>)}
+                </DaysInput>
+            </ListItem>
         );
     };
 
@@ -75,11 +107,50 @@ export const Habits = () => {
                     <EntryButton onClick={openEntry}>+</EntryButton>
                 </TitleContainer>
                 {isOpen && Entry()}
+                {/* <Message>
+                    Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!
+                </Message> */}
+                <List>
+                    {dummyHabits.map(h => <ListOfHabits 
+                        key={h.id}
+                        name={h.name}
+                        days={h.days}
+                    />)}
+                </List>
             </HabitsContainer>
             <Footer />
         </>
     );
 };
+
+const List = styled.ul`
+    
+`;
+const Trashcan = styled.img`
+    width: 13px;
+    height: 15px;
+    position: absolute;
+    right: 10px;
+    top: 11px;
+`;
+
+const SubTitle = styled.h2`
+    font-family: ${PRIMARY_FONT};
+    font-size: 20px;
+    color: ${DARK_GREY};
+    padding-top: 13px;
+    padding-bottom: 10px;
+`;
+
+const ListItem = styled.li`
+    width: 340px;
+    height: 91px;
+    background-color: #ffffff;
+    border-radius: 5px;
+    margin: 10px auto;    
+    padding-left: 15px;
+    position: relative;
+`;
 
 const HabitsContainer = styled.div`
     height: 100vh;
@@ -93,6 +164,7 @@ const TitleContainer = styled.div`
     align-items: center;
     justify-content: space-between;
     margin-top: 70px;
+    margin-bottom: 30px;
     padding-top: 28px;
 `;
 
@@ -123,7 +195,7 @@ const DaysInput = styled.div`
     display: flex;
 `;
 
-const Day = styled.li`
+const Day = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
@@ -162,4 +234,10 @@ const SaveButton = styled.button`
     border: none;
     border-radius: 5px;
     margin-left: 20px;    
+`;
+
+const Message = styled.p`
+    font-family: ${PRIMARY_FONT};
+    font-size: 18px;
+    color: ${DARK_GREY};   
 `;
