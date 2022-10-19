4319d42f177a6c8fd07a8f56491bce0da44110a9
Laisses <laisse@gmail.com>
2022-10-19 16:40:42 -0300

    add layout to History component

--- src/components/History.js
+++ src/components/History.js
@@ -1,5 +1,28 @@
-export const History = () => {    
+import styled from "styled-components";
+import { useState } from "react";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { Title, Container, Message } from "./Common";
+
+export const History = () => {
     return (
-        <div>Sou o History</div>
+        <>
+            <Header />
+            <Container>
+                <TitleContainer>
+                    <Title>Histórico</Title>
+                </TitleContainer>
+                <Message>
+                    Em breve você poderá ver o histórico dos seus hábitos aqui!
+                </Message>
+            </Container>
+            <Footer />
+        </>
     );
 };
+
+const TitleContainer = styled.div`
+    margin-top: 70px;
+    margin-bottom: 17px;
+    padding-top: 28px;
+`;
