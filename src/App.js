db19c88f1053b5ae518ac8b46da928e69c498988
Laisses <laisse@gmail.com>
2022-10-19 21:59:15 -0300

    context API setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { GlobalStyle } from "./assets/styles/GlobalStyle";
+import { useState } from "react";
 import { Login } from "./components/Login";
 import { SignUp } from "./components/SignUp";
 import { Habits } from "./components/Habits";
 import { Today } from "./components/Today";
 import { History } from "./components/History";
+import { AppContext } from "./components/context";
 
 function App() {
+  
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/cadastro" element={<SignUp />} />
-        <Route path="/habitos" element={<Habits />} />
-        <Route path="/hoje" element={<Today />} />
-        <Route path="/historico" element={<History />} />
-      </Routes>
-    </BrowserRouter>
-
+    <AppContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastro" element={<SignUp />} />
+          <Route path="/habitos" element={<Habits />} />
+          <Route path="/hoje" element={<Today />} />
+          <Route path="/historico" element={<History />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,61 @@
 import styled from "styled-components";
 import logo from "../assets/images/logo.png";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { AppContext } from "./context";
 import { useNavigate } from "react-router-dom";
-import { ThreeDots } from "react-loader-spinner";
 import { BASE_URL } from "./constants";
-import { LIGHT_BLUE } from "./constants";
 import { Link } from "react-router-dom";
-import { BigButton, LinkContainer, TextInput, Logo } from "./Common";
+import { BigButton, LinkContainer, TextInput, Logo, Loading } from "./Common";
 
 export const Login = () => {
+
+    const [form, setForm] = useState({ email: "", password: "" });
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const {} = useContext(AppContext)
+
+    const handleForm = e => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+    };
+
+    const signIn = () => {
+        const body = {...form};
+
+        axios.post(`${BASE_URL}/auth/login`,body)
+            .then(res => {
+                setLoading(false);
+                navigate("/hoje");
+            })
+            .catch(err => {
+                alert(err.response.data.message);                
+                setLoading(false);
+            });
+        
+        setLoading(true);
+    }
+
     return (
         <LoginContainer>
             <Logo src={logo} alt="Track It logo" />
-            <TextInput placeholder="email" />
-            <TextInput placeholder="senha" />
+            <TextInput
+                data-identifier="input-email"
+                name="email"
+                value={form.email}
+                placeholder="email"
+                onChange={handleForm}
+                disabled={loading}
+            />
+            <TextInput
+                data-identifier="input-password"
+                name="password"
+                value={form.password}
+                placeholder="senha"
+                onChange={handleForm}
+                disabled={loading}
+                type="password"
+            />
             <BigButton>
                 <Link to="/habitos">
                     Entrar
